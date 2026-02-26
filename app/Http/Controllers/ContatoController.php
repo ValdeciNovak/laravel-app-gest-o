c38fa624e37534d36176a4eaa68eae29502ef21f
ValdeciNovak <valdecirnovak@gmail.com>
2026-02-25 22:29:41 -0300

    feedback do formulário

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,32 +9,34 @@ use App\MotivoContato;
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
-     
-     $motivo_contatos = MotivoContato::all();
 
-    return view('site.contato', ['titulo' => 'Contato (salvo)', 'motivo_contatos' => $motivo_contatos   ]);
+        $motivo_contatos = MotivoContato::all();
+
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request) {
 
+        //realizar a validação dos dados do formulário recebidos no request
         $request->validate([
-            'nome' => 'required|min:3|max:40',
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
+            'email' => 'required|email',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ], [
-            'nome.required' => 'O campo nome é obrigatório.',
-            'nome.min' => 'O campo nome deve conter no mínimo 3 caracteres.',
-            'nome.max' => 'O campo nome deve conter no máximo 40 caracteres.',
-            'telefone.required' => 'O campo telefone é obrigatório.',
-            'email.required' => 'O campo email é obrigatório.',
-            'motivo_contato.required' => 'O campo motivo contato é obrigatório.',
-            'mensagem.required' => 'O campo mensagem é obrigatório.',
-            'mensagem.max' => 'O campo mensagem deve conter no máximo 2000 caracteres.'
+        ],[
+            'nome.required' => 'O campo nome deve ser preenchido',
+            'nome.min' => 'O campo nome deve conter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve conter no máximo 40 caracteres',
+            'nome.unique' => 'O nome informado já está em uso',
+            'telefone.required' => 'O campo telefone deve ser preenchido',
+            'email.required' => 'O campo email deve ser preenchido',
+            'email.email' => 'O email informado não é válido',
+            'motivo_contatos_id.required' => 'O campo motivo contato deve ser preenchido',
+            'mensagem.required' => 'O campo mensagem deve ser preenchido',
+            'mensagem.max' => 'O campo mensagem deve conter no máximo 2000 caracteres'
         ]);
-
         SiteContato::create($request->all());
-        return redirect()->route('site.contato');
+        return redirect()->route('site.index');
     }
 }
