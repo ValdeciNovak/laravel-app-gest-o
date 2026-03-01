f8a97ba3fee3cdd0adf7b5bc01289379f5fcfe12
ValdeciNovak <valdecirnovak@gmail.com>
2026-03-01 12:12:24 -0300

    Edição de forncedores

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -26,7 +26,7 @@ class FornecedorController extends Controller
 
         $msg = '';
 
-        if($request->input('_token') != '') {
+        if($request->input('_token') != '' && $request->input('id') == '') {
             //validacao
             $regras = [
                 'nome' => 'required|min:3|max:40',
@@ -54,6 +54,24 @@ class FornecedorController extends Controller
             //dados view
             $msg = 'Cadastro realizado com sucesso';
         }
+        if($request->input('_token') != '' && $request->input('id') != '') {
+            $fornecedor = Fornecedor::find($request->input('id'));
+            $update =  $fornecedor->update($request->all());
+
+            if($update) {
+                $msg = 'Atualização realizada com sucesso';
+            } else {
+                $msg = 'Erro ao atualizar o registro';
+            }
+            return redirect()->route('app.fornecedor.editar', ['id' => $request->input('id'), 'msg' => $msg]);
+        }
         return view('app.fornecedor.adicionar', ['msg' => $msg]);
+    }
+
+    public function editar($id, $msg = '') {
+
+    $fornecedor = Fornecedor::find($id);
+    return view('app.fornecedor.adicionar', ['fornecedor' => $fornecedor, 'msg' => $msg]);
+
     }
 }
